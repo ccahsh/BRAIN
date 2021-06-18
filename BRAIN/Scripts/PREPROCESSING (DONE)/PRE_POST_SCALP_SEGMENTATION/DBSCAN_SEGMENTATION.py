e7c69c9bb3fbf5b61e5e2919223d4d2c8c9011c6
tlsgusdn1107 <30178389+tlsgusdn1107@users.noreply.github.com>
2021-06-18 10:50:03 +0900

    Add files via upload

---
+++ BRAIN/Scripts/PREPROCESSING (DONE)/PRE_POST_SCALP_SEGMENTATION/DBSCAN_SEGMENTATION.py
@@ -0,0 +1,343 @@
+
+#from line 284 - Comment 1: Ranges for epsilon value and minimum points have to be optimized with GPU support because the code takes more than 2 hours on laptop.
+
+import os
+import vtk
+import shutil
+import numpy as np
+import nibabel as nib
+from pathlib import Path
+from sklearn import metrics
+import plotly.express as px
+import matplotlib.pyplot as plt
+from nipype.interfaces import fsl
+import plotly.graph_objects as go
+from sklearn.cluster import DBSCAN
+from nipype.testing import example_data
+from mpl_toolkits.mplot3d import Axes3D
+
+
+
+# Before: Identification
+
+introduction = input("\nYou need to give cluster classification number, pre-operative and post-operative SCALP files. MNI152 raw file is optional. Press enter to continue.\n\n")
+
+cluster_label = input("\n(REQUIRED) Type in the number you would like to classify the segmented FLAP files with, from 000 to 999. Make sure the number is not repeated in other cluster folders.\n\n")
+
+preop_name = input("\n(REQUIRED) Type in the name of the pre-operative SCALP file. Make sure you include nii.gz format.\n\n")
+
+postop_name = input("\n(REQUIRED) Type in the name of the post-operative SCALP file. Make sure you include nii.gz format.\n\n")
+
+MNI_registration = input("\n(OPTIONAL) Type Y if you would like to register pre-operative and post-operative SCALP files to MNI152 SCALP file. If not, press enter instead.\n\n")
+
+# Optional: Task 0 : MNI Scalp Extraction and Input Normalization
+
+# Comment on Task 0 : From testing, Task 0 may not give significant effect on scalp segmentaation.
+
+if (MNI_registration == Y):
+
+# Task 0.1 : MNI brain segmentation.
+
+    MNI_original = input("\nType in the name of the unprocessed MNI152 file.\n\n")
+    os.rename(MNI_original, "MNI-template.nii.gz")
+    os.system("hd-bet -i MNI-template.nii.gz -device cpu -mode fast -tta 0")
+
+#remove "-device cpu -mode fast -tta 0" if GPU support is available.
+#install HD-BET from https://github.com/MIC-DKFZ/HD-BET. HD-BET is the most up-to-date brain segmentation algorithm (6/17/21).
+
+# Task 0.2 : Making MNI SCALP file.
+
+    os.remove("MNI-template_bet.nii.gz")
+    os.rename("MNI-template_bet_mask.nii.gz", "MNI-template_BRAINMASK.nii.gz")
+
+    brain_mask = nib.load('MNI-template_BRAINMASK.nii.gz')
+    MNI_ref = nib.load('MNI-template.nii.gz')
+
+    brain_mask_A = np.array(brain_mask.dataobj)
+    MNI_ref_A = np.array(MNI_ref.dataobj)
+
+    if(brain_mask_A.shape == MNI_ref_A.shape):
+        
+        for x in range(0, brain_mask_A.shape[0]-1):
+            for y in range(0, brain_mask_A.shape[1]-1):
+                for z in range(0, brain_mask_A.shape[2]-1):
+                    if(brain_mask_A[x][y][z] > 0):
+                        MNI_ref_A[x][y][z] = 0
+                
+    else:
+
+        print("Comparison not possible due to difference in dimensions.")
+        
+    for x in range(0, MNI_ref_A.shape[0]-1):
+        for y in range(0, MNI_ref_A.shape[1]-1):
+            for z in range(0, MNI_ref_A.shape[2]-1):
+                if(x < ((MNI_ref_A.shape[0]-1)*0.03) or x > ((MNI_ref_A.shape[0]-1)*0.96) or y < ((MNI_ref_A.shape[1]-1)*0.01) or y > ((MNI_ref_A.shape[1]-1)*0.99) or z < ((-(MNI_ref_A.shape[2]-1)*y*0.000275)+85)):
+                        MNI_ref_A[x][y][z] = 0
+                    
+    def paraMAX():
+        M = 0
+        for x in range(int(0.05*(MNI_ref_A.shape[0]-1)),int(0.95*(MNI_ref_A.shape[0]-1))):
+            for y in range(int(0.05*(MNI_ref_A.shape[1]-1)),int(0.95*(MNI_ref_A.shape[1]-1))):
+                for z in range(int(0.05*(MNI_ref_A.shape[2]-1)),int(0.95*(MNI_ref_A.shape[2]-1))):
+                   if(M < MNI_ref_A[x][y][z]):
+                        M = MNI_ref_A[x][y][z]
+        return M
+        
+    MAX = paraMAX()
+    MAX_thres = MAX*0.225
+
+    for x in range(0, MNI_ref_A.shape[0]-1):
+        for y in range(0, MNI_ref_A.shape[1]-1):
+            for z in range(0, MNI_ref_A.shape[2]-1):
+                if(MNI_ref_A[x][y][z] < MAX_thres):
+                    MNI_ref_A[x][y][z] = 0
+     
+    MNI_scalp_array = nib.Nifti1Image(MNI_ref_A, affine=np.eye(4))
+    nib.save(MNI_scalp_array, "MNI-template_SCALP.nii.gz")
+
+# Task 0.3 : Aligning pre-operative and post-operative SCALP files onto MNI SCALP file.
+
+    flt1 = fsl.FLIRT(bins=640, cost_func='mutualinfo')
+    flt1.inputs.in_file = preop_name
+    flt1.inputs.reference = 'MNI-template_SCALP.nii.gz'
+    flt1.inputs.output_type = "NIFTI_GZ"
+    flt1.cmdline
+    res = flt1.run()
+
+    os.remove(str(preop_name[:-7]) + '_flirt.mat')
+    preop_name = str(preop_name[:-7]) + '_flirt.nii.gz'
+    
+    flt2 = fsl.FLIRT(bins=640, cost_func='mutualinfo')
+    flt2.inputs.in_file = postop_name
+    flt2.inputs.reference = 'MNI-template_SCALP.nii.gz'
+    flt2.inputs.output_type = "NIFTI_GZ"
+    flt2.cmdline
+    res = flt2.run()
+
+    os.remove(str(postop_name[:-7]) + '_flirt.mat')
+    preop_name = str(postop_name[:-7]) + '_flirt.nii.gz'
+
+
+
+# Task 1 : Intensity Normalization and Difference NIFTI Generation
+
+preop = nib.load(preop_name) #might need to change to normalized name
+A = np.array(preop.dataobj)  #might need to change to normalized name
+
+postop = nib.load(postop_name)
+B = np.array(postop.dataobj)
+
+normal_diff = np.zeros((A.shape[0],A.shape[1],A.shape[2]))
+
+print(np.max(A))
+print(np.max(B))
+
+AA = A/(np.max(A))
+BB = B/(np.max(B))
+
+MIN_thres = 0.34
+
+# Aligning postoperative SCALP file to preoperative SCALP file lowers accuracy, so it is avoided in this script. Minimum threshold for absolute intensity difference is manually selected after testing with pre-operative and post-operative T1w files (Patient 3) in OPENNEURO Database: https://openneuro.org/datasets/ds001226/versions/1.0.0 and https://openneuro.org/datasets/ds002080/versions/1.0.1. This is subject to change, depending on whether T1w is registered with T2w before analysis or not.
+
+for x in range(0,AA.shape[0]-1):
+    for y in range(0,AA.shape[1]-1):
+        for z in range(0,AA.shape[2]-1):
+            if abs(AA[x][y][z]-BB[x][y][z] > MIN_thres):
+                normal_diff[x][y][z] = 1
+            else:
+                normal_diff[x][y][z] = 0
+                
+normal_diff_nib = nib.Nifti1Image(normal_diff, affine=np.eye(4))
+nib.save(normal_diff_nib, "normalized_difference_MNIenabled.nii.gz")
+
+# Completion 1 notified.
+
+confirmation1 = input("\nNormalized, unprocessed FLAP File generated and ready for mesh. Press enter to continue.")
+
+
+
+# Task 2 : NIFTI to Mesh Conversion
+
+# Task 2.1 : NIFTI to Mesh
+
+def nii_2_mesh(filename_nii, filename_stl, label):
+
+    reader = vtk.vtkNIFTIImageReader()
+    reader.SetFileName(filename_nii)
+    reader.Update()
+    
+    surf = vtk.vtkDiscreteMarchingCubes()
+    surf.SetInputConnection(reader.GetOutputPort())
+    surf.SetValue(0, label)
+    surf.Update()
+    
+    smoother= vtk.vtkWindowedSincPolyDataFilter()
+    if vtk.VTK_MAJOR_VERSION <= 5:
+        smoother.SetInput(surf.GetOutput())
+    else:
+        smoother.SetInputConnection(surf.GetOutputPort())
+    smoother.SetNumberOfIterations(30)
+    smoother.NonManifoldSmoothingOn()
+    smoother.NormalizeCoordinatesOn()
+    smoother.GenerateErrorScalarsOn()
+    smoother.Update()
+     
+    writer = vtk.vtkSTLWriter()
+    writer.SetInputConnection(smoother.GetOutputPort())
+    writer.SetFileTypeToASCII()
+    writer.SetFileName(filename_stl)
+    writer.Write()
+    
+if __name__ == '__main__':
+
+    X = 'normalized_difference_MNIenabled.nii.gz'
+    
+    Y = X[:-7] + '.stl'
+    
+    filename_nii =  X
+    filename_stl = Y
+    label = 1
+    nii_2_mesh(filename_nii, filename_stl, label)
+    
+# Task 2.2 : Mesh Relocation
+
+os.mkdir('Mesh_Visualization_' + str(cluster_label))
+shutil.move('normalized_difference_MNIenabled.stl','Mesh_Visualization_' + str(cluster_label))
+
+# Completion 2 notified.
+
+confirmation2 = input("Normalized, unprocessed FLAP mesh file generated and relocated. Ready for DBSCAN. Press enter to continue.")
+
+
+
+# Task 3 : DBSCAN Segmentation
+
+a = nib.load('rescaled_difference_MNIenabled.nii.gz')
+A = np.array(a.dataobj)
+
+# Task 3.1 : Listing coordinates of rescaled_difference NIFTI file into arrays
+
+valid_coord = []
+
+for x in range(0,A.shape[0]-1):
+    for y in range(0,A.shape[1]-1):
+        for z in range(0,A.shape[2]-1):
+            if (A[x][y][z] == 1):
+                valid_coord += [[x,y,z]]
+
+#print(valid_coord)
+
+
+# Task 3.2 : Rescaling coordinates into [0,1]^3 volume.
+
+X = [v[0] for v in valid_coord]
+Y = [v[1] for v in valid_coord]
+Z = [v[2] for v in valid_coord]
+
+# Extra: plotting coordinate information into graph for visualization
+
+#fig, axs = plt.subplots(1, 3, figsize=(16,4), dpi=300)
+
+#axs[0].hist(X, bins=50, color='black', rwidth=0.9)
+#axs[0].set_title('X Coordinates')
+#axs[1].hist(Y, bins=50, color='black', rwidth=0.9)
+#axs[1].set_title('Y Coordinates')
+#axs[2].hist(Z, bins=50, color='black', rwidth=0.9)
+#axs[2].set_title('Z Coordinates')
+
+X_rescaled = []
+m = np.max(X)
+for i in range(len(X)):
+    X_rescaled.append(X[i]/m)
+
+Y_rescaled = []
+n = np.max(Y)
+for j in range(len(Y)):
+    Y_rescaled.append(Y[j]/n)
+    
+Z_rescaled = []
+p = np.max(Z)
+for k in range(len(Z)):
+    Z_rescaled.append(Z[k]/p)
+
+#print(X_rescaled)
+#print(Y_rescaled)
+#print(Z_rescaled)
+
+valid_coord_rescaled = []
+for e in range(len(X_rescaled)):
+    valid_coord_rescaled.append([X_rescaled[e],Y_rescaled[e],Z_rescaled[e]])
+
+#print(valid_coord_rescaled)
+
+
+# Task 3.3 : Executing DBSCAN
+# Note: HDBSCAN may be implemented if necessary.
+
+# Task 3.3.1 : Setting up silhouette scores and combinations of epsilon / minimum samples
+
+S = []
+comb = []
+
+eps_r = range(5,75)
+minpts_r = range(3,10)
+
+# Comment 1: Ranges for epsilon value and minimum points have to be optimized with GPU support because the code takes more than 2 hours on laptop.
+# Comment 2: k_max may not have a value for eps_r = range(6,12) and minpts_r = range(3,8) because "TypeError: unsupported operand type(s) for /: 'str' and 'int'" appeared in terminal after running the code with the parameters mentioned.
+
+for k in eps_r:
+    for j in minpts_r:
+        model = DBSCAN(eps = k*0.01, min_samples = j)
+        run = model.fit(valid_coord_rescaled)
+        S.append(metrics.silhouette_score(valid_coord_rescaled, run.labels_, metric='euclidean'))
+        comb.append([str(k),str(j)])
+        
+
+# Task 3.3.2 : Finding maximum silhouette scores and corresponding [epsilon / minimum samples] values.
+
+Smax = 0
+Smax_set = []
+
+for q in range(len(eps_r)*len(minpts_r)):
+    if (S[q] > Smax):
+        Smax = S[q]
+        Smax_set = comb[q]
+        
+# Assume silhouette coefficient will be 1 mode for max. If not, the code is still valid because the epsilon value is smallest.
+
+k_max = Smax_set[0]
+j_max = Smax_set[1]
+
+
+# Task 3.3.3 : Performing DBSCAN
+
+maxmodel = DBSCAN(eps = k_max*0.01, min_samples = j_max, metric = 'euclidean', metric_params = None, algorithm='auto', leaf_size = 30, p = None, n_jobs = None)
+
+# All parameters included for potential adjustments.
+
+scanned_results = maxmodel.fit(valid_coord_rescaled)
+confirmation33 = input("DBSCAN complete. Press enter to continue.")
+
+
+# Task 3.4 : Creating and storing cluster-based files for GT selection.
+
+os.mkdir('Clusters_' + str(cluster_label))
+
+cluster_max = max(scanned_results.labels_)
+
+cluster_lists = [[] for i in range(cluster_max+1)]
+
+for l in range(len(scanned_results)):
+    if (scanned_results.labels_[l] >= 0):
+        cluster_lists[scanned_results.labels_[l]].append(valid_coord[l])
+
+for r in range(cluster_max+1):
+    cluster_indi = np.array(cluster_lists[r])
+    cluster_coord = np.zeros((A.shape[0], A.shape[1], A.shape[2]))
+    for s in range(len(cluster_indi)):
+        cluster_coord[cluster_indi[s][0],cluster_indi[s][1],cluster_indi[s][2]] = 1
+    cluster_nib = nib.Nifti1Image(cluster_coord, affine=np.eye(4))
+    nib.save(cluster_nib, "DBSCAN-cluster" + str(r) + ".nii.gz"))
+    shutil.move("DBSCAN-cluster" + str(r) + ".nii.gz", 'Clusters_' + str(cluster_label))
+    
+# Cluster files created and saved in 'Clusters' folder.
---
+++ BRAIN/Scripts/PREPROCESSING (DONE)/SINGLE_SET_ANALYSIS/SCALP_BRAIN_SEGMENTATION.py
@@ -0,0 +1,330 @@
+
+# def functions are not used purposefully. Code will be condensed once it is approved.
+# pip3 is assumed to be installed. Replace pip3 with pip in if pip is used instead.
+# First few lines of code will install and setup HD-BET from https://github.com/MIC-DKFZ/HD-BET and DeepBrainSeg from https://github.com/koriavinash1/DeepBrainSeg. HD-BET is the most up-to-date brain segmentation algorithm (6/16/21). DeepBrainSeg has a pretrained model for tumor segmentation that could also be run in Macbook Pro. Delete or comment on lines 21-26 once HD-BET and DeepBrainSeg are installed.
+# Line 201 comment : FLIRT before brain extraction vs. FLIRT after brain extraction?
+# Line 328 comment: Tumor segmentation has not been done yet due to poor processing speed in Macbook Pro. Confirming the location of saved files is necessary.
+
+
+import os
+import numpy as np
+import nibabel as nib
+from nipype.interfaces import fsl
+from nipype.testing import example_data
+
+
+
+# Task 0 : Reminding the requirements and installing HD-BET / DeepBrainSeg.
+
+print("\nAll MNI files must be in the same directory as the python script without any additional subfolders. T1w and MNI152 reference files must be included, whereas T2w file is recommended.\n\n")
+
+os.system("git clone https://github.com/MIC-DKFZ/HD-BET")
+os.system("cd HD-BET")
+os.system("pip3 install -e .")
+os.system("cd ..")
+
+os.system("pip3 install DeepBrainSeg")
+
+from DeepBrainSeg import deepSeg
+
+# Task 1 : Query. T1w and MNI152 files are mandatory, while T2w files are optional.
+
+T1w_name = input("\n(REQUIRED) Type in the name of the input T1w file. Make sure you include nii.gz format.\n\n")
+T2w_name = input("\n(OPTIONAL) Type in the name of the input T2w file. Make sure you include nii.gz format. Write down N/A if T2w file is not applicable or available.\n\n")
+MNI_name = input("\n(REQUIRED) Type in the name of the MNI152 reference file. Make sure you include nii.gz format.\n\n")
+
+print("\nInput complete.\n")
+
+os.rename(MNI_name, "MNI-template.nii.gz")
+os.rename(T1w_name, "input-t1w.nii.gz")
+
+if(T2w_name == 'N/A'):
+    MNIreplace = nib.load('MNI-template.nii.gz')
+    MNIreplace_array = np.array(MNIreplace.dataobj)
+    replace = np.zeros((MNIreplace_array.shape[0], MNIreplace_array.shape[1], MNIreplace_array.shape[2]))
+    replace_nib = nib.Nifti1Image(replace, affine=np.eye(4))
+    nib.save(replace_nib, "input-t2w.nii.gz")
+else:
+    os.rename(T2w_name, "input-t2w.nii.gz")
+    
+# Completion 1 notified.
+
+confirmation1 = input("\nFile labels standardized for alignment. Press enter to continue.")
+        
+        
+        
+# Task 2 : Alignment of T1w and T2w files to MNI152 file.
+
+flt = fsl.FLIRT(bins=640, cost_func='mutualinfo')
+flt.inputs.in_file = 'input-t1w.nii.gz'
+flt.inputs.reference = 'MNI-template.nii.gz'
+flt.inputs.output_type = "NIFTI_GZ"
+flt.cmdline
+res = flt.run()
+
+os.remove("input-t1w_flirt.mat")
+
+flt2 = fsl.FLIRT(bins=640, cost_func='mutualinfo')
+flt2.inputs.in_file = 'input-t2w.nii.gz'
+flt2.inputs.reference = 'MNI-template.nii.gz'
+flt2.inputs.output_type = "NIFTI_GZ"
+flt2.cmdline
+res = flt2.run()
+
+os.remove("input-t2w_flirt.mat")
+
+flt3 = fsl.FLIRT(bins=640, cost_func='mutualinfo')
+flt3.inputs.in_file = 'input-t2w_flirt.nii.gz'
+flt3.inputs.reference = 'input-t1w_flirt.nii.gz'
+flt3.inputs.output_type = "NIFTI_GZ"
+flt3.cmdline
+res = flt3.run()
+
+os.remove("input-t2w_flirt_flirt.mat")
+os.rename("input-t2w_flirt_flirt.nii.gz", "input-t2w_flirt_t1w.nii.gz")
+
+os.remove("input-t2w_flirt.nii.gz")
+
+# Completion 2 notified.
+
+confirmation2 = input("Files registered for overlay. Press enter to continue.")
+
+
+
+# Task 3 : Overlay of MNI-aligned T1 and T2
+
+processedt1w = nib.load('input-t1w_flirt.nii.gz')
+processedt2w = nib.load('input-t2w_flirt_t1w.nii.gz')
+
+processedt1w_array = np.array(processedt1w.dataobj)
+processedt2w_array = np.array(processedt2w.dataobj)
+
+if (processedt1w_array.shape == processedt2w_array.shape):
+    confirmation31 = input("\nOverlay possible. Press enter to continue.\n")
+    t1w_t2w_array = np.add(processedt1w_array, processedt2w_array)
+    t1w_t2w_overlay = nib.Nifti1Image(t1w_t2w_array, affine=np.eye(4))
+    nib.save(t1w_t2w_overlay, "t1w_t2w_overlay_MNIenabled.nii.gz")
+    
+else:
+    print("Overlay not possible. Check dimensions.")
+
+# Completion 3 notified.
+
+confirmation3 = input("Files ready for brain mask segmentation. Press enter to continue.")
+
+
+
+# Task 4 : Brain Mask Segmentation with HD-BET.
+
+os.system("hd-bet -i t1w_t2w_overlay_MNIenabled.nii.gz -device cpu -mode fast -tta 0")
+
+#remove "-device cpu -mode fast -tta 0" if GPU support is available.
+
+os.remove("t1w_t2w_overlay_MNIenabled_bet.nii.gz")
+os.rename("t1w_t2w_overlay_MNIenabled_bet_mask.nii.gz", "t1w_t2w_overlay_MNIenabled_BRAINMASK.nii.gz")
+
+# Completion 4 notified. MNI-normalized BRAIN MASK EXTRACTION COMPLETE.
+
+confirmation4 = input("MNI-normalized BRAIN MASK EXTRACTION COMPLETE. Saved as t1w_t2w_overlay_MNIenabled_BRAINMASK.nii.gz. Files ready for scalp segmentation. Press enter to continue.")
+
+
+
+# Task 5 : Manual Scalp Segmentation. Parameters may be changed as necessary.
+
+brain_mask = nib.load('t1w_t2w_overlay_MNIenabled_BRAINMASK.nii.gz')
+t1w_t2w = nib.load('t1w_t2w_overlay_MNIenabled.nii.gz')
+
+brain_mask_A = np.array(brain_mask.dataobj)
+t1w_t2w_A = np.array(t1w_t2w.dataobj)
+
+# 5.1 : Checking dimensional congruency between brain mask and overlaid file.
+
+if(brain_mask_A.shape == t1w_t2w_A.shape):
+
+# 5.2 : Removing brain from overlaid file.
+    
+    for x in range(0, brain_mask_A.shape[0]-1):
+        for y in range(0, brain_mask_A.shape[1]-1):
+            for z in range(0, brain_mask_A.shape[2]-1):
+                if(brain_mask_A[x][y][z] > 0):
+                    t1w_t2w_A[x][y][z] = 0
+            
+else:
+
+    print("Comparison not possible due to difference in dimensions.")
+    
+# 5.3 : Isolating scalp with enclosed coordinate volume.
+
+for x in range(0, t1w_t2w_A.shape[0]-1):
+    for y in range(0, t1w_t2w_A.shape[1]-1):
+        for z in range(0, t1w_t2w_A.shape[2]-1):
+            if(x < ((t1w_t2w_A.shape[0]-1)*0.03) or x > ((t1w_t2w_A.shape[0]-1)*0.96) or y < ((t1w_t2w_A.shape[1]-1)*0.01) or y > ((t1w_t2w_A.shape[1]-1)*0.99) or z < ((-(t1w_t2w_A.shape[2]-1)*y*0.000275)+85)):
+                t1w_t2w_A[x][y][z] = 0
+                
+# 5.4 : Finding value of threshold intensity for scalp segmentation.
+
+def paraMAX():
+    M = 0
+    for x in range(int(0.05*(t1w_t2w_A.shape[0]-1)),int(0.95*(t1w_t2w_A.shape[0]-1))):
+        for y in range(int(0.05*(t1w_t2w_A.shape[1]-1)),int(0.95*(t1w_t2w_A.shape[1]-1))):
+            for z in range(int(0.05*(t1w_t2w_A.shape[2]-1)),int(0.95*(t1w_t2w_A.shape[2]-1))):
+               if(M < t1w_t2w_A[x][y][z]):
+                    M = t1w_t2w_A[x][y][z]
+    return M
+    
+MAX = paraMAX()
+MAX_thres = MAX*0.225
+
+#Multiplication constant was determined from optimizing scalp segmentation from most updated pre-operative and post-operative T1w files (Patient 3) in OPENNEURO Database: https://openneuro.org/datasets/ds001226/versions/1.0.0 and https://openneuro.org/datasets/ds002080/versions/1.0.1. Patient 3 has right-sided (lateral view), parietal meningioma I. Full patient information available at Supplementary Table 1. Patient characteristics. from https://onlinelibrary.wiley.com/doi/abs/10.1002/pon.5195. For scalp segmentation, T2w files may be recommended to not be included in the input for reducing intensity values of scalp regions.
+
+# 5.5 : Segmenting scalp using threshold intensity.
+
+for x in range(0, t1w_t2w_A.shape[0]-1):
+    for y in range(0, t1w_t2w_A.shape[1]-1):
+        for z in range(0, t1w_t2w_A.shape[2]-1):
+            if(t1w_t2w_A[x][y][z] < MAX_thres):
+                t1w_t2w_A[x][y][z] = 0
+ 
+scalp_array = nib.Nifti1Image(t1w_t2w_A, affine=np.eye(4))
+nib.save(scalp_array, "t1w_t2w_overlay_MNIenabled_SCALP.nii.gz")
+
+# Completion 5 notified. MNI-normalized SCALP EXTRACTION COMPLETE.
+
+confirmation5 = input("MNI-normalized SCALP EXTRACTION COMPLETE. Saved as t1w_t2w_overlay_MNIenabled_SCALP.nii.gz. Files ready for tumor segmentation. For this segmentation, {T1w, T2w, T1ce, FLAIR} files are required. Press enter to continue.")
+
+
+
+# Task 6 : Brain Tumor Segmentation with DeepBrainSeg. Pretrained weights will be used.
+# Comment : nnuNet is ideal for brain tumor segmentation, but a simpler package is used instead so as to be implementable in Macbook Pro. Code may be changed if Hospital Intranet makes nnUNet installation possible.
+
+# Task 6.1 : Brain extraction from MNI-aligned T1w, T2w, T1ce, and FLAIR files.
+# Comment : FLIRT before brain extraction vs. FLIRT after brain extraction?
+
+# T1w
+os.system("hd-bet -i input-t1w_flirt.nii.gz -device cpu -mode fast -tta 0")
+#remove "-device cpu -mode fast -tta 0" if GPU support is available.
+os.rename("input-t1w_flirt_bet.nii.gz", "input-t1w_flirt_BRAIN.nii.gz")
+os.remove("input-t1w_flirt_bet_mask.nii.gz")
+
+T1w_pretumor_name = "input-t1w_flirt_BRAIN.nii.gz"
+
+# T2w
+if(T2w_name == 'N/A'):
+
+    T2w_name = input("\n(REQUIRED) Type in the name of the input T2w file. Make sure you include nii.gz format. You cannot write N/A for this one. If you wrote N/A previously, then avoid writing input-t2w.nii.gz for this one.\n\n")
+    
+    flt1 = fsl.FLIRT(bins=640, cost_func='mutualinfo')
+    flt1.inputs.in_file = T2w_name
+    flt1.inputs.reference = 'MNI-template.nii.gz'
+    flt1.inputs.output_type = "NIFTI_GZ"
+    flt1.cmdline
+    res = flt1.run()
+    
+    os.remove(str(T2w_name[:-7]) + "_flirt.mat")
+    
+    T2w_name = str(T2w_name[:-7]) + "_flirt.nii.gz"
+    
+    flt2 = fsl.FLIRT(bins=640, cost_func='mutualinfo')
+    flt2.inputs.in_file = T2w_name
+    flt2.inputs.reference = 'input-t1w_flirt.nii.gz'
+    flt2.inputs.output_type = "NIFTI_GZ"
+    flt2.cmdline
+    res = flt2.run()
+
+    os.remove(str(T2w_name[:-7]) + "_flirt.mat")
+    os.rename(str(T2w_name[:-7]) + "_flirt.nii.gz", str(T2w_name[:-7]) + "_t1w.nii.gz")
+    
+    T2w_name = str(T2w_name[:-7]) + "_t1w.nii.gz"
+    
+else:
+
+    T2w_name = "input-t2w_flirt_t1w.nii.gz"
+
+os.system("hd-bet -i " + str(T2w_name) + " -device cpu -mode fast -tta 0")
+#remove "-device cpu -mode fast -tta 0" if GPU support is available.
+os.rename(str(T2w_name[:-7]) + "_bet.nii.gz", str(T2w_name[:-7]) + "_BRAIN.nii.gz")
+os.remove(str(T2w_name[:-7]) + "_bet_mask.nii.gz")
+
+T2w_pretumor_name = str(T2w_name[:-7]) + "_BRAIN.nii.gz"
+
+#T1ce
+
+T1ce_name = input("\n(REQUIRED) Type in the name of the input T1ce file. Make sure you include nii.gz format.\n\n")
+
+flt1 = fsl.FLIRT(bins=640, cost_func='mutualinfo')
+flt1.inputs.in_file = T1ce_name
+flt1.inputs.reference = 'MNI-template.nii.gz'
+flt1.inputs.output_type = "NIFTI_GZ"
+flt1.cmdline
+res = flt1.run()
+
+os.remove(str(T1ce_name[:-7]) + "_flirt.mat")
+
+T1ce_name = str(T1ce_name[:-7]) + "_flirt.nii.gz"
+
+flt2 = fsl.FLIRT(bins=640, cost_func='mutualinfo')
+flt2.inputs.in_file = T1ce_name
+flt2.inputs.reference = 'input-t1w_flirt.nii.gz'
+flt2.inputs.output_type = "NIFTI_GZ"
+flt2.cmdline
+res = flt2.run()
+
+os.remove(str(T1ce_name[:-7]) + "_flirt.mat")
+os.rename(str(T1ce_name[:-7]) + "_flirt.nii.gz", str(T1ce_name[:-7]) + "_t1w.nii.gz")
+
+T1ce_name = str(T1ce_name[:-7]) + "_t1w.nii.gz"
+
+os.system("hd-bet -i " + str(T1ce_name) + " -device cpu -mode fast -tta 0")
+#remove "-device cpu -mode fast -tta 0" if GPU support is available.
+os.rename(str(T1ce_name[:-7]) + "_bet.nii.gz", str(T1ce_name[:-7]) + "_BRAIN.nii.gz")
+os.remove(str(T1ce_name[:-7]) + "_bet_mask.nii.gz")
+
+T1ce_pretumor_name = str(T1ce_name[:-7]) + "_BRAIN.nii.gz"
+ 
+#FLAIR
+
+FLAIR_name = input("\n(REQUIRED) Type in the name of the input FLAIR file. Make sure you include nii.gz format.\n\n")
+
+flt1 = fsl.FLIRT(bins=640, cost_func='mutualinfo')
+flt1.inputs.in_file = FLAIR_name
+flt1.inputs.reference = 'MNI-template.nii.gz'
+flt1.inputs.output_type = "NIFTI_GZ"
+flt1.cmdline
+res = flt1.run()
+
+os.remove(str(FLAIR_name[:-7]) + "_flirt.mat")
+
+FLAIR_name = str(FLAIR_name[:-7]) + "_flirt.nii.gz"
+
+flt2 = fsl.FLIRT(bins=640, cost_func='mutualinfo')
+flt2.inputs.in_file = FLAIR_name
+flt2.inputs.reference = 'input-t1w_flirt.nii.gz'
+flt2.inputs.output_type = "NIFTI_GZ"
+flt2.cmdline
+res = flt2.run()
+
+os.remove(str(FLAIR_name[:-7]) + "_flirt.mat")
+os.rename(str(FLAIR_name[:-7]) + "_flirt.nii.gz", str(FLAIR_name[:-7]) + "_t1w.nii.gz")
+
+FLAIR_name = str(FLAIR_name[:-7]) + "_t1w.nii.gz"
+
+os.system("hd-bet -i " + str(FLAIR_name) + " -device cpu -mode fast -tta 0")
+#remove "-device cpu -mode fast -tta 0" if GPU support is available.
+os.rename(str(FLAIR_name[:-7]) + "_bet.nii.gz", str(FLAIR_name[:-7]) + "_BRAIN.nii.gz")
+os.remove(str(FLAIR_name[:-7]) + "_bet_mask.nii.gz")
+
+FLAIR_pretumor_name = str(FLAIR_name[:-7]) + "_BRAIN.nii.gz"
+ 
+# Location Specification
+
+t1_path = T1w_pretumor_name
+t2_path = T2w_pretumor_name
+t1ce_path = T1ce_pretumor_name
+flair_path = FLAIR_pretumor_name
+
+segmentor = deepSeg(quick=True)
+segmentor.get_segmentation(t1_path, t2_path, t1ce_path, flair_path, save = True)
+
+#Tumor segmentation has not been done yet due to poor processing speed in Macbook Pro. Confirming the location of saved files is necessary.
+#Unlike HD-BET, nnUNet requires GPU, so the code cannot be done in regular laptop. Link to nnUNet: https://github.com/MIC-DKFZ/nnUNet. Installation and setup cannot be done in Macbook Pro as of right now.
+#Other examples: https://github.com/Mr-TalhaIlyas/Brain-Tumor-Segmentation, https://github.com/galprz/brain-tumor-segmentation.
