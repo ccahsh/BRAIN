2b6b91856b424e742456c92ad01ac9a211d5f949
tlsgusdn1107 <30178389+tlsgusdn1107@users.noreply.github.com>
2021-07-07 12:31:39 +0900

    C++ implementation

--- BRAIN/Scripts/PREPROCESSING (DONE)/PRE_POST_SCALP_SEGMENTATION/DBSCAN_SEGMENTATION.py
+++ BRAIN/Scripts/PREPROCESSING (DONE)/PRE_POST_SCALP_SEGMENTATION/DBSCAN_SEGMENTATION.py
@@ -1,16 +1,17 @@
 
-# def functions are not used purposefully. Code will be condensed once it is approved. The code is verbose considering jit, but it is not critical.
-#from line 326 - Comment 1: Ranges for epsilon value and minimum points have to be optimized with GPU support because the code takes more than 2 hours on laptop.
-
 
 import os
+import re
 import vtk
+import sys
+import csv
+import math
 import shutil
 import numpy as np
+import pandas as pd
 import nibabel as nib
 from pathlib import Path
 from sklearn import metrics
-from numba import jit, cuda
 import plotly.express as px
 import matplotlib.pyplot as plt
 from nipype.interfaces import fsl
@@ -20,33 +21,73 @@ from nipype.testing import example_data
 from mpl_toolkits.mplot3d import Axes3D
 
 
-@jit(target ="cuda")
-def DBSCAN_Segmentation():
+# defining nii to stl conversion
 
-    # Before: HD-BET Installation and Identification
+def nii_2_mesh(filename_nii, filename_stl, label):
+
+    try:
+        
+        reader = vtk.vtkNIFTIImageReader()
+        reader.SetFileName(filename_nii)
+        reader.Update()
+        
+        surf = vtk.vtkDiscreteMarchingCubes()
+        surf.SetInputConnection(reader.GetOutputPort())
+        surf.SetValue(0, label)
+        surf.Update()
+        
+        smoother= vtk.vtkWindowedSincPolyDataFilter()
+        if vtk.VTK_MAJOR_VERSION <= 5:
+            smoother.SetInput(surf.GetOutput())
+        else:
+            smoother.SetInputConnection(surf.GetOutputPort())
+        smoother.SetNumberOfIterations(30)
+        smoother.NonManifoldSmoothingOn()
+        smoother.NormalizeCoordinatesOn()
+        smoother.GenerateErrorScalarsOn()
+        smoother.Update()
+         
+        writer = vtk.vtkSTLWriter()
+        writer.SetInputConnection(smoother.GetOutputPort())
+        writer.SetFileTypeToASCII()
+        writer.SetFileName(filename_stl)
+        writer.Write()
+        
+    except:
+        
+        pass
+
+
+
+if __name__ == "__main__":
+
+# Before: HD-BET Installation and Identification
 
     if not os.path.exists("HD-BET"):
 
         os.system("git clone https://github.com/MIC-DKFZ/HD-BET")
         os.system("cd HD-BET")
         os.system("pip3 install -e .")
-        os.system("cd ..")
+        os.system("cd ../")
 
     introduction = input("\nYou need to give cluster classification number, pre-operative and post-operative SCALP files. MNI152 raw file is optional. Press enter to continue.\n\n")
 
-    cluster_label = input("\n(REQUIRED) Type in the number you would like to classify the segmented FLAP files with, from 000 to 999. Make sure the number is not repeated in other cluster folders.\n\n")
+    cluster_label = input("\n(REQUIRED) Type in the number you would like to classify the segmented FLAP files with, from 000 to 999. Make sure the number is not repeated in other cluster folders. If repeated, the folder will be overwritten.\n\n")
 
+    if not os.path.exists('Clusters_' + str(cluster_label)):
+        os.mkdir('Clusters_' + str(cluster_label))
+        
     preop_name = input("\n(REQUIRED) Type in the name of the pre-operative SCALP file. Make sure you include nii.gz format.\n\n")
 
     postop_name = input("\n(REQUIRED) Type in the name of the post-operative SCALP file. Make sure you include nii.gz format.\n\n")
 
-    MNI_registration = input("\n(OPTIONAL) Type Y if you would like to register pre-operative and post-operative SCALP files to MNI152 SCALP file. If not, press enter instead. We recommend MNI normalization so as to make sure the GT models are congruent to the MRI template scalp file, which may be used as input for training algorithm.\n\n")
+    MNI_registration = input("\n(OPTIONAL) Type Y if you would like to register pre-operative and post-operative SCALP files to MNI152 SCALP file. If not, press enter instead. We recommend MNI normalization so as to make the GT models congruent to the MRI template scalp file, which may be used as input for training algorithm.\n\n")
 
     # Optional: Task 0 : MNI Scalp Extraction and Input Normalization
 
     # Comment on Task 0 : From testing, Task 0 may not give significant effect on scalp segmentaation.
 
-    if (MNI_registration == Y):
+    if (MNI_registration == 'Y'):
 
     # Task 0.1 : MNI brain segmentation.
 
@@ -104,7 +145,7 @@ def DBSCAN_Segmentation():
         # Task 0.2.4 : Filtering by maximum threshold.
         
         MAX = paraMAX()
-        MAX_thres = MAX*0.225
+        MAX_thres = 0.225*MAX
         
         for x in range(0, MNI_ref_A.shape[0]-1):
             for y in range(0, MNI_ref_A.shape[1]-1):
@@ -114,7 +155,7 @@ def DBSCAN_Segmentation():
         
         # Task 0.2.5 : Removing non-scalp voxels by area inspection.
         
-        ns_thres = 0.34
+        ns_thres = 0.34*MAX
         
         for x in range(1, MNI_ref_A.shape[0]-1):
             for y in range(1, MNI_ref_A.shape[1]-1):
@@ -157,7 +198,7 @@ def DBSCAN_Segmentation():
 
 
 
-    # Task 1 : Intensity Normalization and Difference NIFTI Generation
+    # Task 1 : Intensity Normalization, Difference NIFTI Generation, and Mesh
 
     preop = nib.load(preop_name) #might need to change to normalized name
     A = np.array(preop.dataobj)  #might need to change to normalized name
@@ -167,15 +208,12 @@ def DBSCAN_Segmentation():
 
     normal_diff = np.zeros((A.shape[0],A.shape[1],A.shape[2]))
 
-    print(np.max(A))
-    print(np.max(B))
-
     AA = A/(np.max(A))
     BB = B/(np.max(B))
 
     MIN_thres = 0.34
 
-    # Aligning postoperative SCALP file to preoperative SCALP file lowers accuracy, so it is avoided in this script. Minimum threshold for absolute intensity difference is manually selected after testing with pre-operative and post-operative T1w files (Patient 3) in OPENNEURO Database: https://openneuro.org/datasets/ds001226/versions/1.0.0 and https://openneuro.org/datasets/ds002080/versions/1.0.1. This is subject to change, depending on whether T1w is registered with T2w before analysis or not.
+    # Aligning postoperative SCALP file to preoperative SCALP file lowers accuracy, so it is avoided in this script. Minimum threshold for absolute intensity difference is manually selected after testing with pre-operative and post-operative T1w files (Patient 3) in OPENNEURO Database: https://openneuro.org/datasets/ds001226/versions/1.0.0 and https://openneuro.org/datasets/ds002080/versions/1.0.1. This is subject to change, depending on whether T1w is registered with T2w before analysis or not. However, skull segmentation is better with T1w only (confirmed).
 
     for x in range(0,AA.shape[0]-1):
         for y in range(0,AA.shape[1]-1):
@@ -188,73 +226,25 @@ def DBSCAN_Segmentation():
     normal_diff_nib = nib.Nifti1Image(normal_diff, affine=np.eye(4))
     nib.save(normal_diff_nib, "normalized_difference_MNIenabled.nii.gz")
 
+    filename_nii = 'normalized_difference_MNIenabled.nii.gz'
+    filename_stl = filename_nii[:-7] + '.stl'
+    label = 1
+
+    nii_2_mesh(filename_nii, filename_stl, label)
+    shutil.move('normalized_difference_MNIenabled.stl','Clusters_' + str(cluster_label))
+
     # Completion 1 notified.
 
-    confirmation1 = input("\nNormalized, unprocessed FLAP File generated and ready for mesh. Press enter to continue.")
+    confirmation1 = input("\nNormalized, unprocessed FLAP File generated and mesh generated (relocated). Press enter to continue.")
 
 
 
-    # Task 2 : NIFTI to Mesh Conversion
+    # Task 2 : DBSCAN Segmentation (C++ implementation)
 
-    # Task 2.1 : NIFTI to Mesh
+    print("\nFor DBSCAN segmentation, epsilon value is 8, and minpoints value is 40. For eps and minpts, edit main.cpp if needed. For size thresholds, edit this script.")
 
-    def nii_2_mesh(filename_nii, filename_stl, label):
-
-        reader = vtk.vtkNIFTIImageReader()
-        reader.SetFileName(filename_nii)
-        reader.Update()
-        
-        surf = vtk.vtkDiscreteMarchingCubes()
-        surf.SetInputConnection(reader.GetOutputPort())
-        surf.SetValue(0, label)
-        surf.Update()
-        
-        smoother= vtk.vtkWindowedSincPolyDataFilter()
-        
-        if vtk.VTK_MAJOR_VERSION <= 5:
-        
-            smoother.SetInput(surf.GetOutput())
-            
-        else:
-        
-            smoother.SetInputConnection(surf.GetOutputPort())
-        smoother.SetNumberOfIterations(30)
-        smoother.NonManifoldSmoothingOn()
-        smoother.NormalizeCoordinatesOn()
-        smoother.GenerateErrorScalarsOn()
-        smoother.Update()
-         
-        writer = vtk.vtkSTLWriter()
-        writer.SetInputConnection(smoother.GetOutputPort())
-        writer.SetFileTypeToASCII()
-        writer.SetFileName(filename_stl)
-        writer.Write()
-
-    filename_nii = 'normalized_difference_MNIenabled.nii.gz'
-        
-    filename_stl = filename_nii[:-7] + '.stl'
-        
-    label = 1
-        
-    nii_2_mesh(filename_nii, filename_stl, label)
-        
-    # Task 2.2 : Mesh Relocation
-
-    os.mkdir('Mesh_Visualization_' + str(cluster_label))
-    shutil.move('normalized_difference_MNIenabled.stl','Mesh_Visualization_' + str(cluster_label))
-
-    # Completion 2 notified.
-
-    confirmation2 = input("Normalized, unprocessed FLAP mesh file generated and relocated. Ready for DBSCAN. Press enter to continue.")
-
-
-
-    # Task 3 : DBSCAN Segmentation
-
-    a = nib.load('rescaled_difference_MNIenabled.nii.gz')
+    a = nib.load('normalized_difference_MNIenabled.nii.gz')
     A = np.array(a.dataobj)
-
-    # Task 3.1 : Listing coordinates of rescaled_difference NIFTI file into arrays
 
     valid_coord = []
 
@@ -264,136 +254,81 @@ def DBSCAN_Segmentation():
                 if (A[x][y][z] == 1):
                     valid_coord += [[x,y,z]]
 
-    #print(valid_coord)
+    np.savetxt("valid_coord.csv", valid_coord, delimiter=",")
 
+    with open('valid_coord.csv', newline='') as csvfile:
+        data = np.array(list(csv.reader(csvfile)))
+        data_float = data.astype(float)
 
-    # Task 3.2 : Rescaling coordinates into [0,1]^3 volume.
+    add = np.array([[data_float.shape[0], ' ', ' ']])
 
-    X = [v[0] for v in valid_coord]
-    Y = [v[1] for v in valid_coord]
-    Z = [v[2] for v in valid_coord]
+    DBSCAN_prep = np.concatenate((add, data_float))
 
-    # Extra: plotting coordinate information into graph for visualization
+    np.savetxt("valid_coord.dat", DBSCAN_prep, fmt='%s', delimiter=',')
 
-    #fig, axs = plt.subplots(1, 3, figsize=(16,4), dpi=300)
+    os.remove("valid_coord.csv")
 
-    #axs[0].hist(X, bins=50, color='black', rwidth=0.9)
-    #axs[0].set_title('X Coordinates')
-    #axs[1].hist(Y, bins=50, color='black', rwidth=0.9)
-    #axs[1].set_title('Y Coordinates')
-    #axs[2].hist(Z, bins=50, color='black', rwidth=0.9)
-    #axs[2].set_title('Z Coordinates')
+    df = pd.read_csv("valid_coord.dat", sep=";", header=0)
 
-    X_rescaled = []
-    m = np.max(X)
-    for i in range(len(X)):
-        X_rescaled.append(X[i]/m)
+    df.rename(columns = lambda x: re.sub('\D','',x),inplace=True)
 
-    Y_rescaled = []
-    n = np.max(Y)
-    for j in range(len(Y)):
-        Y_rescaled.append(Y[j]/n)
+    df.to_csv("valid_coord_DBSCANprep.dat", sep = ' ', index = False)
+
+    os.remove("valid_coord.dat")
+
+    os.system("g++ main.cpp dbscan.cpp -o DBSCANcsv")
+
+    os.system("./DBSCANcsv")
+
+    os.remove("valid_coord_DBSCANprep.dat")
+
+    # Cluster organization (cluster_label, Ashape, and valid_coord given from input)
+
+    DBSCANraw = np.genfromtxt('DBSCAN_raw.csv')
+
+    rownum = int(DBSCANraw.shape[0]/4 - 1)
+
+    cluster_max = 0
+
+    for i in range(rownum + 1):
+        if DBSCANraw[4*i + 3] >= cluster_max:
+            cluster_max = int(DBSCANraw[4*i + 3])
         
-    Z_rescaled = []
-    p = np.max(Z)
-    for k in range(len(Z)):
-        Z_rescaled.append(Z[k]/p)
+    cluster_lists = [[] for i in range(cluster_max + 1)]
 
-    #print(X_rescaled)
-    #print(Y_rescaled)
-    #print(Z_rescaled)
-
-    valid_coord_rescaled = []
-    for e in range(len(X_rescaled)):
-        valid_coord_rescaled.append([X_rescaled[e],Y_rescaled[e],Z_rescaled[e]])
-      
-    valid_coord_rescaled = np.array(valid_coord_rescaled)
-
-    #print(valid_coord_rescaled)
-
-
-    # Task 3.3 : Executing DBSCAN
-    # Note: HDBSCAN may be implemented if necessary.
-
-    # Task 3.3.1 : Setting up silhouette scores and combinations of epsilon / minimum samples
-
-    S = []
-    comb = []
-
-    eps_r = range(5,75)
-    minpts_r = range(3,200)
-
-    # Comment 1: Ranges for epsilon value and minimum points have to be optimized with GPU support because the code takes more than 2 hours on laptop to run once.
-    # Comment 2: Epsilon value should be at least 6, and minimum points should be greater than 10. The optimal values cannot be calculated given comment 1 above.
-
-    for k in eps_r:
-        for j in minpts_r:
-            model = DBSCAN(eps = int(k)*0.01, min_samples = int(j))
-            run = model.fit(valid_coord_rescaled)
-            S.append(metrics.silhouette_score(valid_coord_rescaled, run.labels_, metric='euclidean'))
-            comb.append([str(k),str(j)])
-            
-
-    # Task 3.3.2 : Finding maximum silhouette scores and corresponding [epsilon / minimum samples] values.
-
-    Smax = 0
-    Smax_set = []
-
-    for q in range(len(eps_r)*len(minpts_r)):
-        if (S[q] > Smax):
-            Smax = S[q]
-            Smax_set = comb[q]
-            
-    # Assume silhouette coefficient will be 1 mode for max. If not, the code is still valid because the epsilon value is smallest.
-
-    k_max = Smax_set[0]
-    #print(k_max)
-    j_max = Smax_set[1]
-    #print(j_max)
-
-
-    # Task 3.3.3 : Performing DBSCAN
-
-    maxmodel = DBSCAN(eps = int(k_max)*0.01, min_samples = int(j_max), metric = 'euclidean', metric_params = None, algorithm='auto', leaf_size = 30, p = None, n_jobs = None)
-
-    # All parameters included for potential adjustments.
-
-    scanned_results = maxmodel.fit(valid_coord_rescaled)
-    confirmation33 = input("DBSCAN complete. Press enter to continue.")
-
-
-    # Task 3.4 : Creating and storing cluster-based files for GT selection.
-
-    os.mkdir('Clusters_' + str(cluster_label))
-
-    cluster_max = max(scanned_results.labels_)
-
-    cluster_lists = [[] for i in range(cluster_max+1)]
-
-    for l in range(len(scanned_results)):
-        if (scanned_results.labels_[l] >= 0):
-            cluster_lists[scanned_results.labels_[l]].append(valid_coord[l])
-
-    for r in range(cluster_max+1):
+    for i in range(rownum + 1):
+        if DBSCANraw[4*i + 3] >= 1:
+            cluster_lists[int(DBSCANraw[4*i + 3])].append([valid_coord[i]])
+        
+    for r in range(1,cluster_max + 1):
         cluster_indi = np.array(cluster_lists[r])
         cluster_coord = np.zeros((A.shape[0], A.shape[1], A.shape[2]))
         for s in range(len(cluster_indi)):
-            cluster_coord[cluster_indi[s][0],cluster_indi[s][1],cluster_indi[s][2]] = 1
-        cluster_nib = nib.Nifti1Image(cluster_coord, affine=np.eye(4))
-        nib.save(cluster_nib, "DBSCAN-cluster_" + str(r) + ".nii.gz")
-
-        filename_nii = "DBSCAN-cluster_" + str(r) + ".nii.gz"
+            cluster_coord[cluster_indi[s][0][0],cluster_indi[s][0][1],cluster_indi[s][0][2]] = 1
+        count = 0
+        for x in range(A.shape[0]):
+            for y in range(A.shape[1]):
+                for z in range(A.shape[2]):
+                    if cluster_coord[x][y][z] == 1:
+                        count += 1
+        if count >= 10000:
+            cluster_nib = nib.Nifti1Image(cluster_coord, affine=np.eye(4))
+            nib.save(cluster_nib, "DBSCAN-cluster" + str(r) + ".nii.gz")
             
-        filename_stl = filename_nii[:-7] + '.stl'
-        
-        label = 1
+            filename_nii = "DBSCAN-cluster" + str(r) + ".nii.gz"
+            filename_stl = filename_nii[:-7] + '.stl'
+            label = 1
             
-        nii_2_mesh(filename_nii, filename_stl, label)
-            
-        shutil.move("DBSCAN-cluster_" + str(r) + ".nii.gz", 'Clusters_' + str(cluster_label))
-        shutil.move("DBSCAN-cluster_" + str(r) + ".stl", 'Clusters_' + str(cluster_label))
-        
-    # Cluster files created and saved in 'Clusters_#' folder.
+            nii_2_mesh(filename_nii, filename_stl, label)
+                
+            shutil.move("DBSCAN-cluster" + str(r) + ".nii.gz", 'Clusters_' + str(cluster_label))
+            shutil.move("DBSCAN-cluster" + str(r) + ".stl", 'Clusters_' + str(cluster_label))
+
+    os.remove("DBSCAN_raw.csv")
+    shutil.move('normalized_difference_MNIenabled.nii.gz','Clusters_' + str(cluster_label))
 
 
-DBSCAN_Segmentation()
+
+
+
+
